da67bbb1dde5875a17bd2f18514afe0c8c19937a
Anushka Singh <anushka019bteceai23@igdtuw.ac.in>
2025-08-15 23:56:14 +0530

    more hashing

---
+++ hashing/sess0.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+//--------brute force
+/*
+int maxFreq(vector<int>&arr){
+  int maxCount=0;
+  int maxElement=0;
+  for(int i = 0;i<arr.size();++i){
+    bool before = false;
+    for(int k=0;k<i;++k){
+      if(arr[k]==arr[i]){
+       before = true;
+       break;
+      }
+    }
+    if(before) continue;
+    int count=0;
+    for(int j=0;j<arr.size();++j){
+      if(arr[j] == arr[i]) count++;
+    }
+    if(count>maxCount){
+      maxCount = count;
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+}
+int minFreq(vector<int>&arr){
+  int minCount = INT_MAX;
+    int minElement = 0;
+
+    for (int i = 0; i < arr.size(); ++i) {
+        bool before = false;
+        for (int k = 0; k < i; ++k) {
+            if (arr[k] == arr[i]) {
+                before = true;
+                break;
+            }
+        }
+        if (before) continue;
+
+        int count = 0;
+        for (int j = 0; j < arr.size(); ++j) {
+            if (arr[j] == arr[i]) count++;
+        }
+
+        if (count < minCount) {
+            minCount = count;
+            minElement = arr[i];
+        }
+    }
+
+    return minElement;
+}
+int main(){
+  int n;
+  cin>>n;
+  vector<int> arr(n);
+  for(int i = 0;i<n;++i){
+    cin>>arr[i];
+  }
+
+  cout<<maxFreq(arr)<<endl;
+  cout<<minFreq(arr);
+
+}*/
+//----------optimised
